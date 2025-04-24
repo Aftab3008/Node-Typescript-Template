c7829e9ca48f27ff47e033c9aa10f04d7f14d29e
Aftab3008 <shaikaftab005@mail.com>
2025-04-24 11:03:21 +0530

    Changes in dev script

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+app.get("/", (req, res) => {
+  const name = process.env.NAME || "World";
   res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT || '3000');
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
